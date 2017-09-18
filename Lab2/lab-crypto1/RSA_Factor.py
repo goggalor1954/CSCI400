73cc39cb71c498d11a571104b09649465a1f86f5
James Roesemann <goggalor1954@gmail.com>
2017-09-18 05:19:19 -0400

    .

--- Lab2/lab-crypto1/RSA_Factor.py
+++ Lab2/lab-crypto1/RSA_Factor.py
@@ -24,9 +24,16 @@ def qFind(vulnerable_moduliA,vulnerable_moduliB, gcdA, gcdB, gcdIndex):
  pValue.append(int(vulnerable_moduliB[gcdIndex[1]],16)/int(gcdB[gcdIndex[1]],16))
  return pValue
 
-#given the p value, the q value and the public exponent, returns the private key decrypted private key.
-def getPrivateKey(qVal, pVal, publicExp, gcdIndex, indexSlot):
- return ((int(publicExp,16)**-1)%(int(pVal[gcdIndex[indexSlot]], 16)-1)*qVal[indexSlot]-1)
+#given the p value, the q value, the vulnerable modulus and the public exponent, returns the private key decrypted private key.
+#i figured out whats wrong here. i need to mutiply by the mod, not apply the mod function %
+def getPrivateKey(qVal, pVal, publicExp, gcdIndex, indexSlot, vulnerable_moduli):
+ #inorder to bugcheck im going to break each of these into its own components
+ e= int(publicExp,16)**-1
+ mod= int(vulnerable_moduli[gcdIndex[indexSlot]],16)
+ p=int(pVal[gcdIndex[indexSlot]],16)-1
+ q=(qVal[indexSlot])-1
+ return e*mod*p*q
+#return (int(publicExp,16)**-1)*int(vulnerable_moduli[gcdIndex[indexSlot]],16)*(pVal[gcdIndex[indexSlot]]-1)*(qVal[indexSlot]-1)
 
 #assignes the value of the public Exponent
 def parsExp(contents):
@@ -62,15 +69,15 @@ def findGCD(gcds, mods, vulnerable_moduli):
 os.system("rm vulnerable_moduli")
 os.system("rm gcds")
 
-#converts a hex value to a float
-def float_to_hex(f): 
- return hex(struct.unpack('<I',     struct.pack('<f', f))[0])
+#converts a hex value to a double
+def double_to_hex(f): 
+ return hex(struct.unpack('<Q',struct.pack('<d', f))[0])
 
 #creates a file containing a properly formated RSA Private key
-def getPrivateKeyFile(qVal, pVal, publicExponent, gcdIndex, indexSlot, fileName):
+def getPrivateKeyFile(qVal, pVal, publicExponent, gcdIndex, indexSlot, fileName, vulnerable_moduli):
  outFile=open(fileName, 'w')
  outFile.write("-----BEGIN RSA PRIVATE KEY-----\n")
- outFile.write(str(float_to_hex(getPrivateKey(qVal, pVal, publicExponent, gcdIndex, indexSlot)).split('0x', 1)[1])+'\n')
+ outFile.write(str(double_to_hex(getPrivateKey(qVal, pVal, publicExponent, gcdIndex, indexSlot, vulnerable_moduli)).split('0x', 1)[1])+'\n')
  outFile.write("-----END RSA PRIVATE KEY-----\n")
  outFile.close()
 
@@ -120,6 +127,8 @@ gcdIndex = gcdCheck(gcdA, gcdB)
 #divides the indexed moduli by the indexd gcd's to get the value of q1 and q2
 qValue = qFind(vulnerable_moduliA,vulnerable_moduliB, gcdA, gcdB, gcdIndex)
 
-getPrivateKeyFile( qValue, gcdA, publicExponentA, gcdIndex, 0, 'PrivateKeyA.txt')
-getPrivateKeyFile( qValue, gcdB, publicExponentB, gcdIndex, 1, 'PrivateKeyB.txt')
+getPrivateKeyFile( qValue, gcdA, publicExponentA, gcdIndex, 0, 'PrivateKeyA.txt', vulnerable_moduliA)
+getPrivateKeyFile( qValue, gcdB, publicExponentB, gcdIndex, 1, 'PrivateKeyB.txt', vulnerable_moduliB)
 
+#test
+#print(getPrivateKey(qValue, gcdA, publicExponentA, gcdIndex, 0, vulnerable_moduliA))
